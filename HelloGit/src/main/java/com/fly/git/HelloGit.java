24342df316fe81384436135c43577cc7e0357643
fly412 <flytaylor@163.com>
2020-11-12 20:47:01 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/fly/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.fly.git;
+
+/**
+ * @author fly
+ * @create 2020--11--12--20:37
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
